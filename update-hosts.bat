2685a5b1930ead45a01ed51c1e5864bd71f4c09a
leozhaozheng <leo.zhaozheng@gmail.com>
2025-06-01 21:09:15 +0800

    initial commit

---
+++ update-hosts.bat
@@ -0,0 +1,17 @@
+@echo off
+
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c "^&chr(34)^&"%~0"^&chr(34)^&" ::","%cd%","runas",1)(window.close)&&exit
+
+cd /d %~dp0
+
+curl -k -o sources\04-githubhosts.txt https://hosts.gitcdn.top/hosts.txt
+
+type sources\* >>hosts
+
+pause
+
+move /Y hosts C:\Windows\System32\drivers\etc\hosts
+
+ipconfig /flushdns
+
+pause
